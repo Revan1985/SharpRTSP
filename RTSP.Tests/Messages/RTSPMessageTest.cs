fefdc833b39022915407400b8d3d69af3a051f0b
Nicolas Graziano <nicolas.graziano@gmail.com>
2024-11-19 17:34:47 +0100

    Test seq write

--- RTSP.Tests/Messages/RTSPMessageTest.cs
+++ RTSP.Tests/Messages/RTSPMessageTest.cs
@@ -8,21 +8,24 @@ namespace Rtsp.Messages.Tests
         //Put a name on test to permit VSNunit to handle them.
         [Test]
         [TestCase("OPTIONS * RTSP/1.0", RtspRequest.RequestType.OPTIONS, TestName = "GetRtspMessageRequest-OPTIONS")]
-        [TestCase("SETUP rtsp://audio.example.com/twister/audio.en RTSP/1.0", RtspRequest.RequestType.SETUP, TestName = "GetRtspMessageRequest-SETUP")]
-        [TestCase("PLAY rtsp://audio.example.com/twister/audio.en RTSP/1.0", RtspRequest.RequestType.PLAY, TestName = "GetRtspMessageRequest-PLAY")]
+        [TestCase("SETUP rtsp://audio.example.com/twister/audio.en RTSP/1.0", RtspRequest.RequestType.SETUP,
+            TestName = "GetRtspMessageRequest-SETUP")]
+        [TestCase("PLAY rtsp://audio.example.com/twister/audio.en RTSP/1.0", RtspRequest.RequestType.PLAY,
+            TestName = "GetRtspMessageRequest-PLAY")]
         public void GetRtspMessageRequest(string requestLine, RtspRequest.RequestType requestType)
         {
             RtspMessage oneMessage = RtspMessage.GetRtspMessage(requestLine);
             Assert.That(oneMessage, Is.InstanceOf<RtspRequest>());
 
             var oneRequest = oneMessage as RtspRequest;
-            Assert.That(oneRequest,Is.Not.Null);
+            Assert.That(oneRequest, Is.Not.Null);
             Assert.That(oneRequest.RequestTyped, Is.EqualTo(requestType));
         }
 
         //Put a name on test to permit VSNunit to handle them.
         [Test]
-        [TestCase("RTSP/1.0 551 Option not supported", 551, "Option not supported", TestName = "GetRtspMessageResponse-551")]
+        [TestCase("RTSP/1.0 551 Option not supported", 551, "Option not supported",
+            TestName = "GetRtspMessageResponse-551")]
         public void GetRtspMessageResponse(string requestLine, int returnCode, string returnMessage)
         {
             RtspMessage oneMessage = RtspMessage.GetRtspMessage(requestLine);
@@ -35,6 +38,17 @@ namespace Rtsp.Messages.Tests
                 Assert.That(oneResponse.ReturnCode, Is.EqualTo(returnCode));
                 Assert.That(oneResponse.ReturnMessage, Is.EqualTo(returnMessage));
             });
+        }
+
+        [Test]
+        public void SeqWrite()
+        {
+            RtspMessage oneMessage = new()
+            {
+                CSeq = 123
+            };
+
+            Assert.That(oneMessage.Headers["CSeq"], Is.EqualTo("123"));
         }
     }
 }
